9dab68c26fea52c21ba2355410819bdef6468374
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-16 18:17:37 -0500

    added a change | via Alk

--- vite-project/src/components/Jamer/Jamer.jsx
+++ vite-project/src/components/Jamer/Jamer.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 export class Jamer extends React.Component {
+
+    constructor() { };
+
     render() {
         return <div className="jamer">I am Jamer</div>;
     };
+
 };
