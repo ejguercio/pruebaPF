ab5f28062ffd3de49bcb3231f0e2c249c14c0098
Bachi <romerobasilio@hotmail.com.ar>
2023-07-17 18:12:35 -0300

    Otra prueba

--- vite-project/src/components/Basilio/Basilio.jsx
+++ vite-project/src/components/Basilio/Basilio.jsx
@@ -4,6 +4,7 @@ const Basilio = () => {
         <h1>Componente Basilio</h1>
         <h2>Estamos practicando Git</h2>
         <h2>Vamos de nuevo</h2>
+        <h3>Vamos de nuevoooo!!!</h3>
     </>
 };
 export default Basilio
