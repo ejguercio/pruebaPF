1b81f214369d525862f0d015317bec65fc0a6b56
Edgar Juan Guercio <jguercio@live.com.ar>
2023-07-17 15:32:08 -0300

    otro intenteo

--- vite-project/src/components/Edgar/Edgar.jsx
+++ vite-project/src/components/Edgar/Edgar.jsx
@@ -1,8 +1,11 @@
+
+
 const Edgar = () => {
-    
+    console.log("hola")
     return <>
-        <h1 >Componente Edgar</h1>
-        <h2>componente modificado prueba</h2>
+        <h1 className={css.title}>Componente Edgar</h1>
+        <h2>modifique una vez</h2>
+        <h3>modifique otra</h3>
     </>
 };
 export default Edgar
